87da10dfb15bc66bc87ff35ee975912348202b3f
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-25 10:35:28 -0300

    instalo libreria de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
- 
-
   return (
     <>
-     
+      <div>
+        <h1>Alta de peliculas</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
