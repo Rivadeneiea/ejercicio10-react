44fae2ab1d39dd737e34306fcf7f0775e64431b9
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-25 11:06:46 -0300

    creo componente formulario y funcion para extraer valor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioPelicula from "./components/FormularioPelicula";
 
 function App() {
   return (
     <>
-      <div>
+      <div className="text-center text-danger">
         <h1>Alta de peliculas</h1>
       </div>
+      <FormularioPelicula></FormularioPelicula>
     </>
   );
 }
